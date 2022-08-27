0e902102f43a25013817a81d9c5663719ad75716
sergey-frd <sergey.frd@gmail.com>
2022-08-27 12:15:13 +0300

    220827
    220827

--- !r_ae_System_1.bat
+++ !r_ae_System_1.bat
@@ -1,3 +1,5 @@
 c:
 cd c:\Program Files\Adobe\Adobe After Effects 2022\Support Files\
-AfterFX   -noui -r  "e:\\Git\\P_h_o_t_o\\AE_JSX\\AE_SF\\sf_System_22_1.jsx"
+rem AfterFX   -noui -r  "e:\\Git\\P_h_o_t_o\\AE_JSX\\AE_SF\\sf_System_22_1.jsx"
+rem AfterFX   -noui -r  "e:\\Git\\P_h_o_t_o\\AE_JSX\\AE_SF\\sf_System_22_d1.jsx"
+AfterFX   -noui -r  "e:\\Git\\P_h_o_t_o\\AE_JSX\\AE_SF\\sf_System_22_y2.jsx"
